2b026a83060373e135a5b8a7ff7c00084cc36eae
slash12 <ns@esokia-webagency.com>
2019-08-27 16:32:35 +0400

    vuejs completed

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,21 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import { constants } from 'fs';
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  // return any data
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        },
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    },
-    addTodo(newTodo) {      
-      this.todos = [...this.todos, newTodo];
-    }
+    Header
   }
 }
 </script>
+
 
 <style>
   * {
